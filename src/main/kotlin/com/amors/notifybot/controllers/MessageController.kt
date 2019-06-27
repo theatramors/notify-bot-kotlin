1c03fab7d27b8738f7c09d5c0ce4d9aa4638cf5d
theatramors <themaliciousman@gmail.com>
2019-06-27 17:58:25 +0300

    remove logger

--- src/main/kotlin/com/amors/notifybot/controllers/MessageController.kt
+++ src/main/kotlin/com/amors/notifybot/controllers/MessageController.kt
@@ -1,8 +1,6 @@
 package com.amors.notifybot.controllers
 
 import com.amors.notifybot.dto.Activity
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -10,11 +8,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class MessageController {
 
-    val logger: Logger = LoggerFactory.getLogger(MessageController::class.java)
-
     /**
      * Эндпоинт приема сообщений
      */
     @PostMapping("/api/messages")
-    fun processRequest(@RequestBody activity: Activity) {}
+    fun processRequest(@RequestBody activity: Activity) {
+    }
 }
