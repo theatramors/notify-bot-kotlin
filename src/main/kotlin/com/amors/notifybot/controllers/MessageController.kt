9cfc996f2d0872466a690aa130c082f492d47fac
theatramors <themaliciousman@gmail.com>
2019-06-28 01:57:00 +0300

    updated MessageController endpoint for processing activities

--- src/main/kotlin/com/amors/notifybot/controllers/MessageController.kt
+++ src/main/kotlin/com/amors/notifybot/controllers/MessageController.kt
@@ -1,17 +1,24 @@
 package com.amors.notifybot.controllers
 
 import com.amors.notifybot.dto.Activity
+import com.amors.notifybot.services.ActivityService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class MessageController {
+class MessageController(private val activityService: ActivityService) {
+
+    private val logger: Logger = LoggerFactory.getLogger(MessageController::class.java)
 
     /**
-     * Эндпоинт приема сообщений
+     * Endpoint for receiving messages
      */
     @PostMapping("/api/messages")
-    fun processRequest(@RequestBody activity: Activity) {
+    fun processActivity(@RequestBody activity: Activity) {
+        logger.info("Receiving message")
+        activityService.processActivity(activity)
     }
 }
