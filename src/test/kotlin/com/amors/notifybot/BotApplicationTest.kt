73a9b6a67345998ad48a403b69bb118146b91ada
theatramors <themaliciousman@gmail.com>
2019-06-28 00:55:03 +0300

    add context load test

---
+++ src/test/kotlin/com/amors/notifybot/BotApplicationTest.kt
@@ -0,0 +1,17 @@
+package com.amors.notifybot
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [BotApplication::class])
+class BotApplicationTest {
+
+    @Test
+    fun contextLoadSuccess() {
+        Assert.assertTrue(true)
+    }
+}
